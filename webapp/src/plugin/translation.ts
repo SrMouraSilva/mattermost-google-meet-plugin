d5fc4c23540723d3252ab019227b9b339d4b6f4e
yodapotatofly <r4ph0u@hotmail.fr>
2021-07-07 15:56:43 +0200

    Update translation.ts

--- webapp/src/plugin/translation.ts
+++ webapp/src/plugin/translation.ts
@@ -1,11 +1,16 @@
 import en from 'i18n/en.json';
 import pt from 'i18n/pt_BR.json';
+import fr from 'i18n/fr_FR.json';
 
 export const getTranslations= (locale: string) => {
     switch (locale) {
         case 'pt':
             return pt;
         case 'pt-BR':
+            return pt;
+        case 'fr':
+            return pt;
+        case 'fr-FR':
             return pt;
     }
     return en;
