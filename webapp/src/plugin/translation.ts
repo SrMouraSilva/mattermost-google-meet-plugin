3df541ea5890d13b7ab51c1186bdd26a16d0d74a
Adrian <adrian.missy@onewavestudios.com>
2021-07-14 13:19:01 -0500

    Update translation.ts

--- webapp/src/plugin/translation.ts
+++ webapp/src/plugin/translation.ts
@@ -1,5 +1,6 @@
 import en from 'i18n/en.json';
 import pt from 'i18n/pt_BR.json';
+import de from 'i18n/de_DE.json';
 
 export const getTranslations= (locale: string) => {
     switch (locale) {
@@ -7,6 +8,18 @@ export const getTranslations= (locale: string) => {
             return pt;
         case 'pt-BR':
             return pt;
+        case 'de':
+            return de;
+        case 'de-AT':
+            return de;
+        case 'de-CH':
+            return de;
+        case 'de-DE':
+            return de;
+        case 'de-LI':
+            return de;
+        case 'de-LU':
+            return de;
     }
     return en;
 }
